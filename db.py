ae03537ef1a8cc5fa06b689338e6c85d998b0df9
kyungrae <kyungrae2002@gmail.com>
2026-07-17 18:20:24 +0900

    prepare for actual server hosting

--- db.py
+++ db.py
@@ -10,7 +10,12 @@ DATABASE_URL = (
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT', '5432')}/{os.getenv('DB_NAME')}"
 )
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=1800)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+    connect_args={"sslmode": "require"},
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
